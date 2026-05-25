a60bfbe54b2390de5a8ca133c6be3dd966407ae0
Adam Heiler <heileradamm@gmail.com>
2026-05-24 22:48:51 -0400

    more edits to first 3 pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://adam-heiler.github.io',
-  base: '/nuclear-rankine-cycle',
+  base: '/nuclear-rankine-cycle/',
 });
